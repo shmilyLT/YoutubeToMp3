c9a869928b79b4d0be6b5c7c6304e672e0872d2b
khurramsaeed <5n8BX6dp>
2018-02-27 18:33:00 +0100

    Det virker

--- app/src/main/java/com/ksm/youtubetomp3/MainActivity.java
+++ app/src/main/java/com/ksm/youtubetomp3/MainActivity.java
@@ -1,28 +1,36 @@
 package com.ksm.youtubetomp3;
 
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.CollationElementIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    String sharedText;
-    TextView updateUi;
-    WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        browser = (WebView) findViewById(R.id.activity_main_webview);
-        browser.getSettings().setJavaScriptEnabled(true);
-        browser.setWebChromeClient(new WebChromeClient());
-
-        updateUi = (TextView) findViewById(R.id.activity_main_textview);
         // Get intent, action and MIME type
         Intent intent = getIntent();
         String action = intent.getAction();
@@ -32,34 +40,87 @@ public class MainActivity extends AppCompatActivity {
             if ("text/plain".equals(type)) {
                 handleSendText(intent); // Handle text being sent
             }
+        } else {
+            // Handle other intents, such as being started from the home screen
         }
-
-    }
-
-    public void onPageFinished(WebView view, String url) {
-
-        view.loadUrl("javascript:"
-                + "document.getElementById('input').value = '" + url + "';");
     }
 
     void handleSendText(Intent intent) {
-        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
         if (sharedText != null) {
-            if (sharedText.contains("youtube") || sharedText.contains("youtu.be")) {
-                // Update UI to reflect text being shared
-                fetchMP3();
-            }
-            //updateUi.setText(sharedText);
-
-
+            fetchMP3(sharedText);
         }
     }
 
-    private void fetchMP3() {
+    private void fetchMP3(String sharedText) {
+        System.err.println("SHARED TEXT: "+sharedText);
+        String videoId = youtubeVidId(sharedText);
+        // Instantiate the RequestQueue.
+        new GetUrlContentTask().execute("https://ytmp3.cc");
+        //updateUi.setText(sharedText);
 
-        updateUi.setText(sharedText);
+        //browser.loadUrl("https://ytmp3.cc");
+    }
 
-        browser.loadUrl("https://ytmp3.cc");
-        onPageFinished(browser, sharedText);
+    private class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+        protected String doInBackground(String... urls) {
+            URL url = null;
+            try {
+                url = new URL(urls[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.setRequestMethod("GET");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            try {
+                connection.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            BufferedReader rd = null;
+            try {
+                rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String content = "", line;
+            try {
+                while ((line = rd.readLine()) != null) {
+                    content += line + "\n";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+        protected void onPostExecute(String result) {
+            // this is executed on the main thread after the process is over
+            // update your UI here
+            System.err.println(result);
+        }
+    }
+
+    public static String youtubeVidId(String ytUrl) {
+        String vidId = null;
+        Pattern pattern = Pattern.compile(
+                "^https?://.*(?:youtu.be/|v/|u/\\w/|embed/|watch?v=)([^#&?]*).*$",
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(ytUrl);
+        if (matcher.matches()){
+            vidId = matcher.group(1);
+        }
+        return vidId;
     }
 }
